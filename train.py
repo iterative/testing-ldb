9969cdca0a01ffe0b46abb2723c22b4429dac639
Casper da Costa-Luis <casper.dcl@physics.org>
2022-08-12 13:48:08 +0100

    lint

--- train.py
+++ train.py
@@ -13,19 +13,23 @@ Options:
 # will resize all images to (256, 256) and run them through a cut off ResNet50
 
 import os
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-import tensorflow as tf
-from tensorflow import keras
-from keras import callbacks
-from tqdm.keras import TqdmCallback
-from argopt import argopt
-import numpy as np
 import json
 import sys
 
+import numpy as np
+import tensorflow as tf
+from argopt import argopt
+from keras import callbacks
+from tensorflow import keras
+from tqdm.keras import TqdmCallback
+
 directory = "./data"
-user_data = directory 
-test_data = directory + "/labelbook" # this can be the labelbook, or any other test set you create
+user_data = directory
+test_data = (
+    directory + "/labelbook"
+)  # this can be the labelbook, or any other test set you create
 
 ### DO NOT MODIFY BELOW THIS LINE, THIS IS THE FIXED MODEL ###
 batch_size = 8
@@ -35,7 +39,7 @@ if __name__ == "__main__":
     args = argopt(__doc__).parse_args()
     tf.random.set_seed(args.seed)
     train = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + '/train',
+        user_data + "/train",
         labels="inferred",
         label_mode="categorical",
         class_names=["cat", "dog", "muffin", "croissant"],
@@ -43,11 +47,11 @@ if __name__ == "__main__":
         seed=args.seed,
         batch_size=batch_size,
         image_size=(256, 256),
-        crop_to_aspect_ratio=True
+        crop_to_aspect_ratio=True,
     )
 
     valid = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + '/val',
+        user_data + "/val",
         labels="inferred",
         label_mode="categorical",
         class_names=["cat", "dog", "muffin", "croissant"],
@@ -59,7 +63,9 @@ if __name__ == "__main__":
 
     total_length = ((train.cardinality() + valid.cardinality()) * batch_size).numpy()
     if total_length > 10_000:
-        raise IndexError(f"Dataset size larger than 10,000. Got {total_length} examples")
+        raise IndexError(
+            f"Dataset size larger than 10,000. Got {total_length} examples"
+        )
 
     test = tf.keras.preprocessing.image_dataset_from_directory(
         test_data,
@@ -85,7 +91,7 @@ if __name__ == "__main__":
     x = tf.keras.applications.resnet.preprocess_input(inputs)
     x = base_model(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(4, activation='sigmoid')(x)
+    x = tf.keras.layers.Dense(4, activation="sigmoid")(x)
     model = tf.keras.Model(inputs, x)
 
     model.compile(
@@ -97,20 +103,28 @@ if __name__ == "__main__":
     loss_0, acc_0 = model.evaluate(valid)
     print(f"loss {loss_0}, acc {acc_0}")
 
-
     cbks = []
-    cbks.append(callbacks.ModelCheckpoint(
-        "best_model",
-        monitor="val_accuracy",
-        mode="max",
-        verbose=0,
-        save_best_only=True,
-        save_weights_only=True,
-    ))
+    cbks.append(
+        callbacks.ModelCheckpoint(
+            "best_model",
+            monitor="val_accuracy",
+            mode="max",
+            verbose=0,
+            save_best_only=True,
+            save_weights_only=True,
+        )
+    )
     cbks.append(TqdmCallback())
     if args.patience >= 0:
-      cbks.append(callbacks.EarlyStopping(monitor="val_accuracy", patience=args.patience, mode="max", restore_best_weights=True))
-    
+        cbks.append(
+            callbacks.EarlyStopping(
+                monitor="val_accuracy",
+                patience=args.patience,
+                mode="max",
+                restore_best_weights=True,
+            )
+        )
+
     history = model.fit(
         train,
         validation_data=valid,
